673e3cdd776e1b007e7dab7220c4594176b79bad
Gobbledyglomp <skoobidyboo@pm.me>
2026-02-19 18:01:31 +0100

    feat: add education section

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,6 +3,7 @@
 import { Navbar } from '@/components/navbar';
 import { Hero } from '@/components/hero';
 import { Experience } from '@/components/experience';
+import { Education } from '@/components/education';
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <Hero />
         <div className="mx-auto max-w-4xl px-6">
           <Experience />
+          <Education />
         </div>
       </main>
     </>
